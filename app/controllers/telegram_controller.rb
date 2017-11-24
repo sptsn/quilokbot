a31ea233cda160dd3e24557717744e39d89d5cc5
Alexander Spitsyn <sanspits@yandex.ru>
2017-11-25 01:26:38 +0300

    show params

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -4,7 +4,8 @@ class TelegramController < ApplicationController
 
   def message
     Telegram::SendMessageService.new.call(text: 'asdf', chat_id: '3002462')
-    render nothing: true
+    # render nothing: true
+    render json: { params: params }
   end
 
 end
