25687ecad9d082921567d363a4369957b730615d
Alexander Spitsyn <sanspits@yandex.ru>
2017-11-25 01:22:19 +0300

    skip vefify auth token

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -1,5 +1,7 @@
 class TelegramController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
   def message
     Telegram::SendMessageService.new.call(text: 'asdf', chat_id: '3002462')
     render nothing: true
