a55d9e032d5a7775413c2d57426fe6d070167c93
Alexander Spitsyn <sanspits@yandex.ru>
2017-11-25 01:48:13 +0300

    update telegram id

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -3,6 +3,9 @@ class TelegramController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def message
+    reciever = Resident.find_by(telegram_username: params[:message][:from][:username])
+    reciever.update_attribute(:telegram_id, params[:message][:from][:id]) unless resident.telegram_id
+
     answer = Telegram::BotLogic.new.call(from: params[:message][:from][:username], text: params[:message][:text])
     Telegram::SendMessageService.new.call(text: answer, chat_id: params[:message][:from][:id])
     render nothing: true
