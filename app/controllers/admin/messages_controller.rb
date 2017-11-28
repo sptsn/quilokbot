5fa56bf911a3bbcde27a1eccbd8c3d888247c61f
Alexander Spitsyn <sanspits@yandex.ru>
2017-11-28 23:20:15 +0300

    broadcast

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -5,8 +5,25 @@ class Admin::MessagesController < Admin::BaseController
   def index
   end
 
+  def broadcast
+  end
+
+  def send_broadcast
+    res = []
+    Resident.where(telegram_username: 'aspitsyn').each do |resident|
+      r = send_message_service.call(text: message_params[:text], chat_id: resident.telegram_id)
+      res << [r.try(:status), r.try(:body)]
+    end
+    if res.all?{|r| r.first == 200}
+      flash[:success] = 'Messages sent'
+    else
+      flash[:error] = 'Something goes wrong'
+    end
+    redirect_to admin_broadcast_path
+  end
+
   def send_message
-    res = Telegram::SendMessageService.new.call(text: message_params[:text], chat_id: message_params[:chat_id])
+    res = send_message_service.call(text: message_params[:text], chat_id: message_params[:chat_id])
 
     if res.status == 200
       flash[:success] = 'Message sent'
@@ -18,6 +35,10 @@ class Admin::MessagesController < Admin::BaseController
   end
 
   protected
+
+  def send_message_service
+    @send_message_service ||= Telegram::SendMessageService.new
+  end
 
   def message_params
     params.fetch(:message, {})
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/' => 'home#index'
     resources :residents
+    get 'messages/broadcast' => 'messages#broadcast', as: 'broadcast'
+    post 'messages/broadcast' => 'messages#send_broadcast', as: 'send_broadcast'
     get 'messages' => 'messages#index'
     post 'messages/send' => 'messages#send_message', as: 'send_message'
     resources :transactions, only: :index
