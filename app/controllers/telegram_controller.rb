677584fe158ebac2d963751cacd0aa8c54361e7f
Alexander Spitsyn <sanspits@yandex.ru>
2017-12-23 17:14:52 +0300

    new bot interface

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -6,31 +6,43 @@ class TelegramController < Telegram::Bot::UpdatesController
 
   before_action do
     case payload['text']
-    when 'Оставить заявку'
+    when /заявк/
       handle_order
-    when 'Услуги'
+    when /услуги/
       handle_services
+    when /контакты/
+      handle_contacts
+    else
+      respond_with :message, text: 'Для навигации используйте меню'
     end
+  end
+
+  def handle_contacts
+    text = "☎️ Звоните: [+7(920)182-50-40](tel:+79201825040)
+💻 Наш сайт: quilok.com
+⭐️ Мы в социальных сетях:
+[Вконтакте](https://vk.com/byquilok), [Facebook](https://fb.com/byquilok), [Instagram](https://instagram.com/byquilok)"
+
+    respond_with :message, text: text, parse_mode: 'Markdown', disable_web_page_preview: true
   end
 
   def services_list
     {
-      'Бот' => 'bot',
-      'Сайт' => 'site',
-      'Интернет-магазин' => 'shop',
-      'Реклама' => 'adds',
-      'Дизайн' => 'design'
+      '👾 Создать бота' => 'bot',
+      '💻 Создать сайт-визитку' => 'landing',
+      '🖥 Создать интернет-магазин' => 'shop',
+      '📈 Настроить Яндекс.Директ' => 'direct',
+      '💡 SMM-маркетинг' => 'smm',
+      '📸 Создать виртуальный тур' => 'tour',
+      '➡️ Другой вопрос' => 'other'
     }
   end
 
   def handle_services
+    text = "Мы можем:\n" + services_list.map{|key, value| key}.join("\n")
+
     respond_with :message,
-      text: services_list.map{|key, value| key}.join("\n"),
-      reply_markup: {
-        keyboard: [ [text: 'Оставить заявку'], [text: 'Услуги'] ],
-        resize_keyboard: true,
-        one_time_keyboard: true
-      }
+      text: text
   end
 
   def update_telegram_username
@@ -41,11 +53,10 @@ class TelegramController < Telegram::Bot::UpdatesController
 
   def start(data = nil, *)
     respond_with :message,
-      text: "Привет, это телеграм-бот студии Quilok. Чтобы оставить заявку, нажми кнопку ниже.",
+      text: "Для навигации используйте меню",
       reply_markup: {
-        keyboard: [ [text: 'Оставить заявку'], [text: 'Услуги'] ],
-        resize_keyboard: true,
-        one_time_keyboard: true
+        keyboard: [ [text: '📝 Наши услуги'], [text: '✏️ Оставить заявку'], [text: '📌 Наши контакты'] ],
+        resize_keyboard: true
       }
   end
 
@@ -55,7 +66,7 @@ class TelegramController < Telegram::Bot::UpdatesController
     else
       save_context :wait_for_contact
       respond_with :message,
-        text: 'Отлично, пришли свой контакт чтоб я мог тебя запомнить.',
+        text: 'Напишите свое имя или нажмите кнопку ниже',
         reply_markup: {
           keyboard: [ [text: 'Отправить контакт', request_contact: true] ],
           resize_keyboard: true
@@ -64,24 +75,44 @@ class TelegramController < Telegram::Bot::UpdatesController
   end
 
   context_handler :wait_for_contact do |*words|
-    unless payload['contact'].present?
-      save_context :wait_for_contact
-      respond_with :message, text: 'Просто пришли контакт'
-      return
-    end
+    if payload['contact'].present?
+      client = Client.new(
+        first_name: payload['contact']['first_name'],
+        last_name: payload['contact']['last_name'],
+        phone: payload['contact']['phone_number'],
+        telegram_id: payload['contact']['user_id'],
+        telegram_username: from['username']
+      )
 
+      if client.save
+        respond_services
+      else
+        # save_context :wait_for_contact
+        respond_with :message,
+          text: "Ошибка: #{client.errors.full_messages.first}. Заполни контактные данные и попробуй еще раз."
+      end
+    else
+      session['name'] = words[0]
+      save_context :wait_for_phone
+      respond_with :message,
+        text: 'Теперь пришлите свой номер',
+        reply_markup: {
+          remove_keyboard: true
+        }
+    end
+  end
+
+  context_handler :wait_for_phone do |*words|
     client = Client.new(
-      first_name: payload['contact']['first_name'],
-      last_name: payload['contact']['last_name'],
-      phone: payload['contact']['phone_number'],
-      telegram_id: payload['contact']['user_id'],
+      first_name: session[:name],
+      phone: words[0],
+      telegram_id: from['id'],
       telegram_username: from['username']
     )
-
     if client.save
       respond_services
     else
-      save_context :wait_for_contact
+      # save_context :wait_for_phone
       respond_with :message,
         text: "Ошибка: #{client.errors.full_messages.first}. Заполни контактные данные и попробуй еще раз."
     end
@@ -89,9 +120,8 @@ class TelegramController < Telegram::Bot::UpdatesController
 
   def respond_services
     respond_with :message,
-      text: 'Выбери услугу',
+      text: 'Выберите услугу',
       reply_markup: {
-        remove_keyboard: true,
         inline_keyboard: services_list.map{ |key, value| [text: key, callback_data: value] }
       }
   end
@@ -109,15 +139,19 @@ class TelegramController < Telegram::Bot::UpdatesController
     edit_message :text, text: "Выбрана услуга: #{services_list.key(data)}"
 
     respond_with :message,
-      text: 'Заявка отправлена, скоро мы свяжемся с вами',
-      reply_markup: {
-        keyboard: [ [text: 'Оставить заявку'], [text: 'Услуги'] ],
-        resize_keyboard: true
-      }
+      text: 'Спасибо! Ваша заявка успешно отправлена. Менеджер обработает ее в ближайшее время 👌🏼',
+      reply_markup: default_keyboard
   end
 
 
   protected
+
+  def default_keyboard
+    {
+      keyboard: [ [text: '📝 Наши услуги'], [text: '✏️ Оставить заявку'], [text: '📌 Наши контакты'] ],
+      resize_keyboard: true
+    }
+  end
 
   def sender
     @sender ||= Client.find_by(telegram_id: from['id'])
--- app/decorators/client_decorator.rb
+++ app/decorators/client_decorator.rb
@@ -3,11 +3,11 @@ class ClientDecorator < BaseDecorator
   delegate_all
 
   def display_name
-    [source.first_name.titleize, source.last_name.titleize].join(' ')
+    [source.first_name&.titleize, source.last_name&.titleize].join(' ')
   end
 
   def display_name_with_telegram_username
-    "#{source.first_name.titleize} #{source.last_name.titleize} (#{source.telegram_username})"
+    "#{source.first_name&.titleize} #{source.last_name&.titleize} (#{source.telegram_username})"
   end
 
 
