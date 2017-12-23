460ec60a8487bc644249d4a1b4080a44fafac458
Alexander Spitsyn <sanspits@yandex.ru>
2017-12-24 01:24:35 +0300

    bot fix

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -4,19 +4,6 @@ class TelegramController < Telegram::Bot::UpdatesController
   skip_before_action :verify_authenticity_token, :require_user
   # before_action :update_telegram_username, except: :start
 
-  # before_action do
-  #   case payload['text']
-  #   when /заявк/
-  #     handle_order
-  #   when /услуги/
-  #     handle_services
-  #   when /контакты/
-  #     handle_contacts
-  #   else
-  #     respond_with :message, text: 'Для навигации используйте меню'
-  #   end
-  # end
-
   def message(data)
     case data['text']
     when /заявк/
@@ -39,43 +26,35 @@ class TelegramController < Telegram::Bot::UpdatesController
     respond_with :message, text: text, parse_mode: 'Markdown', disable_web_page_preview: true
   end
 
-  def services_list
-    {
-      '👾 Создать бота' => 'bot',
-      '💻 Создать сайт-визитку' => 'landing',
-      '🖥 Создать интернет-магазин' => 'shop',
-      '📈 Настроить Яндекс.Директ' => 'direct',
-      '💡 SMM-маркетинг' => 'smm',
-      '📸 Создать виртуальный тур' => 'tour',
-      '➡️ Другой вопрос' => 'other'
-    }
-  end
-
   def handle_services
-    text = "Мы можем:\n" + services_list.map{|key, value| key}.join("\n")
+    text = "<b>Мы можем:</b>\n" + services_list.except('➡️ Другой вопрос').map{|key, value| '•' + key[1..-1]}.join("\n")
 
     respond_with :message,
-      text: text
-  end
-
-  def update_telegram_username
-    unless sender&.telegram_username
-      sender.update_attribute(:telegram_username, from['username'])
-    end
+      text: text,
+      parse_mode: 'HTML'
   end
 
   def start(data = nil, *)
     respond_with :message,
       text: "Для навигации используйте меню",
-      reply_markup: {
-        keyboard: [ [text: '📝 Наши услуги'], [text: '✏️ Оставить заявку'], [text: '📌 Наши контакты'] ],
-        resize_keyboard: true
-      }
+      reply_markup: default_keyboard
   end
 
   def handle_order
+    respond_with :message,
+      text: 'Выберите услугу',
+      reply_markup: {
+        inline_keyboard: services_list.map{ |key, value| [text: key, callback_data: value] }
+      }
+  end
+
+  def callback_query(data)
+    session['kind'] = data
+
+    edit_message :text, text: "Выбрана услуга: #{services_list.key(data)}"
+
     if sender.present?
-      respond_services
+      complete_order
     else
       save_context :wait_for_contact
       respond_with :message,
@@ -98,9 +77,8 @@ class TelegramController < Telegram::Bot::UpdatesController
       )
 
       if client.save
-        respond_services
+        complete_order
       else
-        # save_context :wait_for_contact
         respond_with :message,
           text: "Ошибка: #{client.errors.full_messages.first}. Заполни контактные данные и попробуй еще раз."
       end
@@ -123,41 +101,44 @@ class TelegramController < Telegram::Bot::UpdatesController
       telegram_username: from['username']
     )
     if client.save
-      respond_services
+      complete_order
     else
-      # save_context :wait_for_phone
       respond_with :message,
         text: "Ошибка: #{client.errors.full_messages.first}. Заполни контактные данные и попробуй еще раз."
     end
   end
 
-  def respond_services
-    respond_with :message,
-      text: 'Выберите услугу',
-      reply_markup: {
-        inline_keyboard: services_list.map{ |key, value| [text: key, callback_data: value] }
-      }
-  end
-
-  def callback_query(data)
+  def complete_order
     Order.create(
-      kind: data,
+      kind: session['kind'],
       client_id: sender.id
     )
 
     User.all.each do |u|
-      Telegram.bot.send_message text: "Новая заявка от клиента #{sender.decorate.display_name} (#{sender.phone}), тема: #{services_list.key(data)}", chat_id: u.telegram_id
+      Telegram.bot.send_message(
+        text: "Новая заявка от клиента #{sender.decorate.display_name} (#{sender.phone}), тема: #{services_list.key(session['kind'])}",
+        chat_id: u.telegram_id
+      )
     end
-
-    edit_message :text, text: "Выбрана услуга: #{services_list.key(data)}"
 
     respond_with :message,
       text: 'Спасибо! Ваша заявка успешно отправлена. Менеджер обработает ее в ближайшее время 👌🏼',
       reply_markup: default_keyboard
   end
 
+protected
 
-  protected
+  def services_list
+    {
+      '👾 Создать бота' => 'bot',
+      '💻 Создать сайт-визитку' => 'landing',
+      '🖥 Создать интернет-магазин' => 'shop',
+      '📈 Настроить Яндекс.Директ' => 'direct',
+      '💡 SMM-маркетинг' => 'smm',
+      '📸 Создать виртуальный тур' => 'tour',
+      '➡️ Другой вопрос' => 'other'
+    }
+  end
 
   def default_keyboard
     {
@@ -170,4 +151,9 @@ class TelegramController < Telegram::Bot::UpdatesController
     @sender ||= Client.find_by(telegram_id: from['id'])
   end
 
+  # def update_telegram_username
+  #   unless sender&.telegram_username
+  #     sender.update_attribute(:telegram_username, from['username'])
+  #   end
+  # end
 end
